1cb8edf9a62efee12eb3ad791341981f92069946
MarcosSilva13 <marcosslp45@gmail.com>
2024-11-23 11:36:25 -0300

    Feat: create project

---
+++ src/main/java/com/refeisoft/RefeisoftApiApplication.java
@@ -0,0 +1,13 @@
+package com.refeisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefeisoftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefeisoftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refeisoft/RefeisoftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refeisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefeisoftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
