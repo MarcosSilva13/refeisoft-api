7eb53cbd3c3e565084bf84de2de42b7b521cda0d
MarcosSilva13 <marcosslp45@gmail.com>
2024-11-27 17:57:46 -0300

    Feat: add student id mapping

--- src/main/java/com/refeisoft/api/mapper/TransactionMapper.java
+++ src/main/java/com/refeisoft/api/mapper/TransactionMapper.java
@@ -3,10 +3,12 @@ package com.refeisoft.api.mapper;
 import com.refeisoft.api.dto.TransactionResponseDTO;
 import com.refeisoft.domain.entity.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TransactionMapper {
 
+    @Mapping(source = "student.studentId", target = "studentId")
     TransactionResponseDTO toTransactionResponseDTO(Transaction transaction);
 }
