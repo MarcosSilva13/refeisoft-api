efa0166f7ab7988f3375da0ac35fcb24bd29a02b
MarcosSilva13 <marcosslp45@gmail.com>
2024-12-02 12:58:19 -0300

    Feat: add filter student by status

--- src/main/java/com/refeisoft/api/filter/StudentFilter.java
+++ src/main/java/com/refeisoft/api/filter/StudentFilter.java
@@ -1,13 +1,16 @@
 package com.refeisoft.api.filter;
 
 import com.refeisoft.domain.entity.Student;
+import com.refeisoft.domain.enums.Status;
 import org.springframework.data.jpa.domain.Specification;
 
 import static com.refeisoft.domain.specification.StudentSpecs.byStudentName;
+import static com.refeisoft.domain.specification.StudentSpecs.byStudentStatus;
 
-public record StudentFilter(String name) {
+public record StudentFilter(String name, Status status) {
 
     public Specification<Student> toStudentSpecification() {
-        return byStudentName(name);
+        return byStudentName(name)
+                .and(byStudentStatus(status));
     }
 }
--- src/main/java/com/refeisoft/domain/specification/StudentSpecs.java
+++ src/main/java/com/refeisoft/domain/specification/StudentSpecs.java
@@ -1,6 +1,7 @@
 package com.refeisoft.domain.specification;
 
 import com.refeisoft.domain.entity.Student;
+import com.refeisoft.domain.enums.Status;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.ObjectUtils;
 
@@ -9,12 +10,19 @@ public class StudentSpecs {
 
     public static Specification<Student> byStudentName(String name) {
         return (root, query, criteriaBuilder) -> {
-            query.orderBy(criteriaBuilder.asc(root.get("name")));
             if (ObjectUtils.isEmpty(name)) {
-                return criteriaBuilder.conjunction();
+                return null;
             }
-
             return criteriaBuilder.like(root.get("name"), "%" + name + "%");
+        };
+    }
+
+    public static Specification<Student> byStudentStatus(Status status) {
+        return (root, query, criteriaBuilder) -> {
+            if (ObjectUtils.isEmpty(status)) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("status"), status);
         };
     }
 }
